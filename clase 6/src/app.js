69f836f75be1bf843d46115ef30c8597df975081
mscrimaglia <maxiscrimaglia@gmail.com>
2023-02-13 22:36:57 -0300

    productmanager and products url

--- clase 6/src/app.js
+++ clase 6/src/app.js
@@ -1,10 +1,24 @@
+
 const express = require('express');
 
 const app = express();
 const port = 8080;
 
-app.get('/saludo',(req, res) => {
-    res.send('Hello world!');
+app.use(express.urlencoded({extended:true}));
+
+const ProductManager = require('./ProductManager.js');
+let productManager = new ProductManager();
+
+app.get('/products', async (req, res) => {
+
+    let limit = req.query.limit;
+
+    await productManager.getProducts().then( response => {
+        if (limit) {
+            return res.send(response.slice(0,limit));
+        }
+        return res.send(response);
+    });
 })
 
 app.listen(port,() => console.log('Port 8080'))
